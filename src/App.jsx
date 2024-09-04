92269b7e4f06a6557efa071f95c841282293f356
PovilasU <povilas.urbonas85@gmail.com>
2024-09-04 15:07:27 +0300

    added babylon js

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import * as BABYLON from "babylonjs";
 
 function App() {
   const [cartVisible, setCartVisible] = useState(false);
@@ -190,68 +191,116 @@ function App() {
 
       {/* Product Details Modal */}
       {selectedProduct && (
-        <div className="modal show d-block" tabIndex="-1" role="dialog">
-          <div className="modal-dialog modal-lg" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title">{selectedProduct.name}</h5>
-                <button
-                  type="button"
-                  className="close"
-                  onClick={closeProductModal}
-                  aria-label="Close"
-                >
-                  <span aria-hidden="true">&times;</span>
-                </button>
+        <ProductModal product={selectedProduct} onClose={closeProductModal} />
+      )}
+    </>
+  );
+}
+
+function ProductModal({ product, onClose }) {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const engine = new BABYLON.Engine(canvas, true);
+    const scene = new BABYLON.Scene(engine);
+
+    const camera = new BABYLON.ArcRotateCamera(
+      "camera",
+      Math.PI / 2,
+      Math.PI / 2,
+      2,
+      BABYLON.Vector3.Zero(),
+      scene
+    );
+    camera.attachControl(canvas, true);
+
+    const light = new BABYLON.HemisphericLight(
+      "light",
+      new BABYLON.Vector3(1, 1, 0),
+      scene
+    );
+
+    const box = BABYLON.MeshBuilder.CreateBox("box", {}, scene);
+
+    engine.runRenderLoop(() => {
+      scene.render();
+    });
+
+    return () => {
+      engine.dispose();
+    };
+  }, []);
+
+  return (
+    <div className="modal show d-block" tabIndex="-1" role="dialog">
+      <div className="modal-dialog modal-lg" role="document">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">{product.name}</h5>
+            <button
+              type="button"
+              className="close"
+              onClick={onClose}
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div className="modal-body">
+            <div className="row">
+              <div className="col-md-6">
+                <img
+                  src={product.images[0]}
+                  className="img-fluid"
+                  alt={product.name}
+                />
               </div>
-              <div className="modal-body">
-                <div className="row">
-                  <div className="col-md-6">
-                    <img
-                      src={selectedProduct.images[0]}
-                      className="img-fluid"
-                      alt={selectedProduct.name}
-                    />
-                  </div>
-                  <div className="col-md-6">
-                    <h5>Description</h5>
-                    <p>{selectedProduct.description}</p>
-                    <h5>Price</h5>
-                    <p>${selectedProduct.price.toFixed(2)}</p>
-                    <button
-                      className="btn btn-primary"
-                      onClick={() => addToCart(selectedProduct)}
-                    >
-                      Add to Cart
-                    </button>
-                  </div>
-                </div>
-                <div className="row mt-3">
-                  {selectedProduct.images.slice(1).map((image, index) => (
-                    <div key={index} className="col-md-4">
-                      <img
-                        src={image}
-                        className="img-fluid"
-                        alt={`${selectedProduct.name} ${index + 1}`}
-                      />
-                    </div>
-                  ))}
-                </div>
-              </div>
-              <div className="modal-footer">
+              <div className="col-md-6">
+                <h5>Description</h5>
+                <p>{product.description}</p>
+                <h5>Price</h5>
+                <p>${product.price.toFixed(2)}</p>
                 <button
-                  type="button"
-                  className="btn btn-secondary"
-                  onClick={closeProductModal}
+                  className="btn btn-primary"
+                  onClick={() => addToCart(product)}
                 >
-                  Close
+                  Add to Cart
                 </button>
               </div>
             </div>
+            <div className="row mt-3">
+              {product.images.slice(1).map((image, index) => (
+                <div key={index} className="col-md-4">
+                  <img
+                    src={image}
+                    className="img-fluid"
+                    alt={`${product.name} ${index + 1}`}
+                  />
+                </div>
+              ))}
+            </div>
+            <div className="row mt-3">
+              <div className="col-md-12">
+                <canvas
+                  ref={canvasRef}
+                  style={{ width: "100%", height: "400px" }}
+                ></canvas>
+              </div>
+            </div>
+          </div>
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              onClick={onClose}
+            >
+              Close
+            </button>
           </div>
         </div>
-      )}
-    </>
+      </div>
+    </div>
   );
 }
 
